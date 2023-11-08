fc1ad26332d37cefaeecb20a6eee54b010dfa133
emmanuel201290 <33639699+emmanuel201290@users.noreply.github.com>
2023-11-08 08:17:25 -0600

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,7 @@
 
 
 const validaCampos = require('../middlewares/validar-campos');
-const validarJWT = require('../middlewares/validar-jwt');
+const validarJWT = require('../middlewares/Validar-jwt');
 const validaRoles = require('../middlewares/validar-roles');
 
 module.exports = {
